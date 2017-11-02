39727b6f1816f8aa9a9d181292d4d2957fac5335
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-01 22:27:25 -0700

    add syper.py, import libraries, import dataset, adjust missing values

---
+++ Part 1 - Data Preprocessing/spyder.py
@@ -0,0 +1,17 @@
+# Data Preprocessing
+
+# Import Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import Dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Use Mean of col for missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
