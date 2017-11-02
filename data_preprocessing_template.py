bf7857dba511a9f4138636fab2a55d14a216a002
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-02 16:17:59 -0700

    add linear regression.py, edit template file

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -18,4 +18,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 """from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)"""
+x_test = sc_x.transform(x_test)
+sc_y = StandardScaler()
+y_test = sc_y.fit_transform(y_train)"""
