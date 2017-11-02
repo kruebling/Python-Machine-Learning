8f5f34fa745513b779535bbc794c42348c9ec699
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-01 23:13:58 -0700

    scale training a test set

--- Part 1 - Data Preprocessing/spyder.py
+++ Part 1 - Data Preprocessing/spyder.py
@@ -28,3 +28,9 @@ y = label_y.fit_transform(y)
 # Splitting data into Training set and Test set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
