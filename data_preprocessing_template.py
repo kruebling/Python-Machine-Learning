cb089bb622e6321d7644b3b39e5432547252aa0f
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-11-01 23:19:38 -0700

    add data preprocessing template, rename old .py file to tutorial

---
+++ data_preprocessing_template.py
@@ -0,0 +1,21 @@
+# Data Preprocessing
+
+# Import Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import Dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Splitting data into Training set and Test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)"""
